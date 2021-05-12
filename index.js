52e0531541ed3add2e755bab80442ee90594c9bb
Flyp <flyp@inbox.ru>
2021-05-12 19:33:44 +0300

    123123

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const TelegramBotApi = require('node-telegram-bot-api')
 
 const token ='1837552262:AAH_tUfJPTOuhoCwaDVZveIJPjA54sxPlzI'
 
-const bot = new TelegramBotApi(token, {poolling:true})
+const bot = new TelegramBotApi(token, {polling:true})
 
     bot.on('message', msg =>{
+        const text = msg.text;
+        const msgid = msg.chat.id;
+        const user = msg.from.first_name;
+bot.sendMessage(msgid, `${user}: Сказал ${text}` )
+
+
 console.log(msg)
     })
